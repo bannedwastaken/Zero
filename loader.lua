5be1c0053e48e9a3756c4cb144294fcce6fb8148
bannedwastaken <skipidi209@gmail.com>
2025-08-17 03:09:20 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 local games = {
-    ['hypershot'] = 100040622766961
+    ['hypershot'] = 17516596118
 }
 
 local games_path = "https://raw.githubusercontent.com/bannedwastaken/Zero/refs/heads/main/games/"
@@ -7,6 +7,5 @@ for i,v in pairs(games) do
     if game.PlaceId == v then 
         loadstring(game:HttpGet(games_path..i.."/main.lua"))()
     end
-	warn('No game support, Loading universal!')
 	-- Adding universal soon
 end
